c557bb10c2fa193a673bf920a94836cc0a51a8f4
Nikolai Efremov <efremovnikolaus@gmail.com>
2022-12-22 00:41:58 +0100

    made changes

--- src/main/java/lesson01/Animal.java
+++ src/main/java/lesson01/Animal.java
@@ -3,6 +3,11 @@ package lesson01;
 public class Animal {
     protected int runLimit;
     protected int swimLimit;
+    private static int countAnimal;
+
+    public Animal() {
+        countAnimal++;
+    }
 
     public static void main(String[] args) {
 
@@ -13,9 +18,15 @@ public class Animal {
         Dog dog = new Dog("Geniy");
         dog.run(210);
         dog.swim(7);
+
+        System.out.println("Total number of animals: " + getCountAnimal());
     }
 
     public void run(int obstacleLength) {}
 
     public void swim(int obstacleLength) {}
+
+    public static int getCountAnimal() {
+        return countAnimal;
+    }
 }
