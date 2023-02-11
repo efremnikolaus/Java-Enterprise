f0ad3653e5f22d895257805396cca14a324233de
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-02-11 20:31:17 +0100

    update

--- src/main/java/MainLessons/lesson03/FileData.java
+++ src/main/java/MainLessons/lesson03/FileData.java
@@ -1,4 +1,25 @@
 package MainLessons.lesson03;
 
 public class FileData {
+    private String nameOfFile;
+    private int sizeInBytes;
+    private String pathToFile;
+
+    public FileData(String nameOfFile, int sizeInBytes, String pathToFile) {
+        this.nameOfFile = nameOfFile;
+        this.sizeInBytes = sizeInBytes;
+        this.pathToFile = pathToFile;
+    }
+
+    public String getNameOfFile() {
+        return nameOfFile;
+    }
+
+    public long getSizeInBytes() {
+        return sizeInBytes;
+    }
+
+    public String getPathToFile() {
+        return pathToFile;
+    }
 }
--- src/main/java/MainLessons/lesson03/FileNavigator.java
+++ src/main/java/MainLessons/lesson03/FileNavigator.java
@@ -1,30 +1,47 @@
 package MainLessons.lesson03;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class FileNavigator {
-    private String path;
-    private List<String> files;
+    private String pathToFile;
+    private HashMap<String, ArrayList<FileData>> files;
 
-    public FileNavigator(String path) {
-        this.path = path;
-        this.files = new ArrayList<>();
+    public FileNavigator() {
+        this.files = new HashMap<>();
     }
 
-    public String getPath() {
-        return path;
+    void add(String nameOfFile, int sizeInBytes, String pathToFile) {
+        FileData fileData = new FileData(nameOfFile, sizeInBytes, pathToFile);
+        if (files.containsKey(pathToFile)){
+            files.get(pathToFile).add(fileData);
+        }
+        else {
+            ArrayList<FileData> newFiles = new ArrayList<>();
+            newFiles.add(fileData);
+            files.put(pathToFile, newFiles);
+        }
     }
-
-    public List<String> getFiles() {
-        return files;
+    public ArrayList<FileData> find(String pathToFile){
+        if (files.containsKey(pathToFile)){
+            return files.get(pathToFile);
+        }
+        return null;
+        }
+    public ArrayList<FileData> filterBySize(int sizeInBytes) {
+        ArrayList<FileData> filteredFiles = new ArrayList<>();
+        for (FileData file : files) {
+            if (file.getSizeInBytes() <= sizeInBytes) {
+                filteredFiles.add(file);
+            }
+        }
+        return filteredFiles;
     }
-
-    @Override
-    public String   toString() {
-        return "FileNavigator{" +
-                "path='" + path + '\'' +
-                ", files=" + files +
-                '}';
+    void remove(String pathToFile){
+        if (!this.pathToFile.equals(pathToFile)){
+            return;
+        }
+        files.clear();
     }
 }
