cf20a23ed3a99d75679235424f7523f9020395a7
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-03-02 00:34:31 +0100

    leetcode Nr.1

---
+++ src/main/java/MainLessons/lesson06/Palindrome.java
@@ -0,0 +1,17 @@
+package MainLessons.lesson06;
+
+public class Palindrome {
+    public boolean isPalindrome(String s) {
+        String newStr="";
+        String revStr="";
+        for(int i=0; i<s.length(); i++){
+            char ch= s.charAt(i);
+            if((ch>=48 && ch<=57) || (ch>=65 && ch<=90) || (ch>=97 && ch <=122)){
+                if(ch>=65 && ch<=90) ch= (char) (ch+32);
+                newStr += ch;
+                revStr = ch+revStr;
+            }
+        }
+        return (revStr.equals(newStr)) ? true: false;
+    }
+}
